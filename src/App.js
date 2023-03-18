28071a49fe667923f7f9a8a281381a5416a0aa22
tein <92306279+kerenIy@users.noreply.github.com>
2023-03-18 08:06:38 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import "https://kerenIy.github.io/src/App.css";
+import "https://kerenIy.github.io/budie/src/App.css";
 
-import Navigation from "https://kerenIy.github.io/src/components/Navigation";
-import Heading from "https://kerenIy.github.io/src/components/Heading";
+import Navigation from "https://kerenIy.github.io/budie/src/components/Navigation";
+import Heading from "https://kerenIy.github.io/budie/src/components/Heading";
 
-import Partners from "https://kerenIy.github.io/src/components/Partners";
-import Features from "https://kerenIy.github.io/src/components/Features";
+import Partners from "https://kerenIy.github.io/budie/src/components/Partners";
+import Features from "https://kerenIy.github.io/budie/src/components/Features";
 
-import Mobile from "https://kerenIy.github.io/src/components/Mobile";
-import Footer from "https://kerenIy.github.io/src/components/Footer";
+import Mobile from "https://kerenIy.github.io/budie/src/components/Mobile";
+import Footer from "https://kerenIy.github.io/budie/src/components/Footer";
 
 function App() {
   return (
